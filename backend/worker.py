7a27089299800e1707445a9dfc74129df1765685
Varun A S <varunanagodu@gmail.com>
2026-02-17 22:58:43 +0530

    Refactor Celery configuration to use REDIS_URL
    Updated Celery configuration to use environment variable for Redis URL.

--- backend/worker.py
+++ backend/worker.py
@@ -3,11 +3,11 @@ import time
 import os
 
 REDIS_URL = os.getenv("REDIS_URL", "redis://redis:6379/0")
-# Configure Celery to use Redis as the message broker
+
 celery = Celery(
-    "worker",  # This is the name of your Celery application
-    broker="redis://localhost:6379/0",  # This is the Redis connection string
-    backend="redis://localhost:6379/0",  # for storing task results
+    "worker",
+    broker=REDIS_URL,
+    backend=REDIS_URL,
 )
 
 
